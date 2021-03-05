804ef1456778c881e74a6fd11c5394982387382b
altheatanguan <80100893+altheatanguan@users.noreply.github.com>
2021-03-05 19:02:57 +0800

    Update and rename tlefinalpt.html to index.php

--- index.php
+++ index.php
@@ -9,10 +9,8 @@
 This function is perhaps the most visible. Police officers are the first ones contact a criminal or the criminal justice system.
 The police patrols helps preventing/avoid crimes, they investigate incidences of crime and to arrest people suspected of committing crimes. Suspects enter the court system after they've been arrested.<p>
 
-
 <b>COURTS SYSTEM</b></br>
 The court system consists of attorneys, judges, and juries, as well as ancillary staff. The guilt or innocence of a suspect is determined in court. The suspect, now a defendant, is offered the opportunity to defend themself in court as evidence is presented. They are either released or is found to have committed the alleged crime. If they are found guilty, the suspect receives a sentence or punishment based on the criteria set by the judge and by statute. The defendant is turned over to the corrections system after the sentencing/hearing.<p>
-
 
 <b>CORRECTION SYSTEM</b></br>
 The corrections system incorporates all forms of sentencing and punishment. It refers to a network of agencies that administers a jurisdiction's of prisons and n community-based programs, like parole and probation boards; this system is a part of the larger criminal justice system, in which additionally includes the police, prosecution, and courts.It includes incarceration and probation, a convicted criminal is the responsibility of the corrections system until their full sentence is served or commuted.<p></p>
@@ -20,7 +18,7 @@ The corrections system incorporates all forms of sentencing and punishment. It r
 <img src="C:\Users\HP\Downloads\social justice.jpg"width="400" height="500">
 
 <font color="White"><font size="100"><h3>HOPE PRESENT IN THE PANDEMIC</font></h3>
-<b><p>This year has been marked with reflection and change. Some days feel good and others feel strange. We’ve learned to work differently. We try to tune out the stress but sometimes life still feels like a mess. Learn new skills that we thought we never could. We find ways to travel to less congested places and we meet our friends with covered faces. Hopefully this year has been filled with opportunity and we’ve tried to exercise and strengthen our immunity. 2020 hasn’t been like we expected but we’ve found new ways to stay connected and worry less about the very small chores.Things will get better because they usually do. Enjoy what you love and learn something new. Don’t forget to pray and always be kind and try to stay positive and learn to unwind.</b></p>
+<b><p>This year has been marked with reflection and change. Some days feel good and others feel strange. We’ve learned to work differently. We try to tune out the stress but sometimes life still feels like a mess. Learn new skills that we thought we never could. We find ways to travel to less congested places and we meet our friends with covered faces. Hopefully this year has been filled with opportunity and we’ve tried to exercise and strengthen our immunity. 2020 hasn’t been like we expected but we’ve found new ways to stay connected and worry less about the very small chores. Things will get better because they usually do. Enjoy what you love and learn something new. Don’t forget to pray and always be kind and try to stay positive and learn to unwind.</b></p>
 
 <font color="White"><font size="100"><h4>RELEVANCE OF THE DRAWING</font></h4>
 <b><p>The social justice has sometimes given us hope, in terms of giving justice to those who aren't given one. Though our social justice system is corrupt that as long as you have wealth, you have the authority to get awaty with everything. This is why the PH politicians protects the corrupt justice system that they've promised to eradicate because as long as you have wealth and power, you have the authority to get away with everything that you can get away from. But it sometimes gave us hope because some victims received the justice that they deserved.</b></p>
